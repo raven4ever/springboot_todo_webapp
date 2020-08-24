d6cc126701d9ef94fe670cedd2450c015ffb47c5
raven4ever <wrtv23@gmail.com>
2020-08-24 18:19:49 +0300

    initial app

---
+++ src/main/java/com/springboot/jspapp/JspappApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.jspapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JspappApplication.class, args);
+    }
+
+}
